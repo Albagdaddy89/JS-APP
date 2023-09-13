ed702ff4325123a354e83d5b708829573af4d2d7
Albagdaddy89 <122852784+Albagdaddy89@users.noreply.github.com>
2023-09-12 20:55:01 -0400

    pokedex added

--- js-app/js/scripts.js
+++ js-app/js/scripts.js
@@ -1,3 +1,27 @@
-alert('Hello world');
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+// Create a blank array called pokemonList
+let pokemonList = [];
+
+// Add Pokémon objects to the array
+pokemonList.push({
+  name: "Bulbasaur",
+  height: 7,
+  types: ["grass", "poison"]
+});
+
+pokemonList.push({
+  name: "Charizard",
+  height: 17,
+  types: ["fire", "flying"]
+});
+
+pokemonList.push({
+  name: "Squirtle",
+  height: 5,
+  types: ["water"]
+});
+
+pokemonList.push({
+     name: "Pikachu",
+     height: 4,
+    types: ["electric"] });
+   
